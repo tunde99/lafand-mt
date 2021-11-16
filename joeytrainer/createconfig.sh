7c53ba03ba701040b906f732ae9317392be49c55
edAI <edai.official.edai@gmail.com>
2021-11-16 19:54:48 +0100

    tweaks for igbo

--- joeytrainer/createconfig.sh
+++ joeytrainer/createconfig.sh
@@ -1,4 +1,4 @@
 #create config file to train a model over the JW300 en-yo corpus using 10k sp vocabulary 
-python scripts/createconfig.py --source_lang en --target_lang yo --task lafand --model_path  /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/joeynmt/ --data_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/
+python scripts/createconfig.py --source_lang en --target_lang ig --task lafand-en-ig --model_path  /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/joeynmt/ --data_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/
 
-#python scripts/createconfig.py --source_lang yo --target_lang en --task 10kjw300yoenFTN --model_path  /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/joeynmt/ --data_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/ --finetune
+python scripts/createconfig.py --source_lang ig --target_lang en --task lafand-ig-en --model_path  /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/joeynmt/ --data_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/
