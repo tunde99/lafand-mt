c3f59fecab32040311f74cd07d068e75fe5f9409
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2021-11-17 18:33:37 +0000

    Update apply_sp.sh

--- joeytrainer/apply_sp.sh
+++ joeytrainer/apply_sp.sh
@@ -1,2 +1,2 @@
-bash scripts/applysentencepiece.sh fr wol ./data/frwol/ 10000 10k
-bash scripts/applysentencepiece.sh fr wol ./data/frwol/ 20000 20k
+bash scripts/applysentencepiece.sh fr wol /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/ 10000 10k
+bash scripts/applysentencepiece.sh fr wol /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/ 20000 20k
