a02f0c8c48b2b43c90ca20e387dd02bd9c1266db
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2021-11-17 17:38:46 +0000

    Update buildvocab.sh

--- joeytrainer/buildvocab.sh
+++ joeytrainer/buildvocab.sh
@@ -2,6 +2,6 @@ src=fr
 tgt=wol
 #mkdir -p joeynmt/data/frmos/fr_mos/10k/
 
-python joeynmt/scripts/build_vocab.py ./data/frwol/spdata/10k/JW/train.$src ./data/frwol/spdata/10k/JW/train.$tgt --output_path ./data/frwol/spdata/10k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/10k/JW/train.$src /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/10k/JW/train.$tgt --output_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/10k/JW/vocab.txt
 
-python joeynmt/scripts/build_vocab.py ./data/frwol/spdata/20k/JW/train.$src ./data/frwol/spdata/20k/JW/train.$tgt --output_path ./data/frwol/spdata/20k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/20k/JW/train.$src /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/20k/JW/train.$tgt --output_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/20k/JW/vocab.txt
