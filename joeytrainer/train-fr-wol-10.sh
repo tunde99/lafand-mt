c6b754d3b0b4dfb5f3ad8554840204b7497fa05f
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2021-11-17 18:11:37 +0000

    Update train-fr-wol-10.sh

--- joeytrainer/train-fr-wol-10.sh
+++ joeytrainer/train-fr-wol-10.sh
@@ -2,7 +2,7 @@ src=fr #the source language code
 tgt=wol #the target langauge code
 name=lafandFrWol10  #the name of the task as used in the createconfig.sh
 cd joeynmt;
-mkdir -p ./joeynmt/${name}_${src}_${tgt}_transformer/
+mkdir -p /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/${name}_${src}_${tgt}_transformer/
 
 #srun -p batch --ntasks 1 --gpus-per-task 1 python3 -m joeynmt train configs/transformer_${name}_${src}_${tgt}.yaml 
 python3 -m joeynmt train configs/transformer_${name}_${src}_${tgt}.yaml 
