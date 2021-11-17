8a383be11a0b63bdc3110270bf7d9d5db9af3c5e
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2021-11-17 18:20:36 +0000

    Update train.sh

--- joeytrainer/train.sh
+++ joeytrainer/train.sh
@@ -1,8 +1,8 @@
-src=en #the source language code
-tgt=ig #the target langauge code
-name=lafand-en-ig  #the name of the task as used in the createconfig.sh
+src=fr #the source language code
+tgt=wol #the target langauge code
+name=lafand-fr-wol  #the name of the task as used in the createconfig.sh
 cd joeynmt;
-mkdir -p /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/joeynmt/${name}_${src}_${tgt}_transformer/
+mkdir -p /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/${name}_${src}_${tgt}_transformer/
 
 #srun -p batch --ntasks 1 --gpus-per-task 1 python3 -m joeynmt train configs/transformer_${name}_${src}_${tgt}.yaml 
 python3 -m joeynmt train configs/transformer_${name}_${src}_${tgt}.yaml 
