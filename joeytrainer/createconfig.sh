8a9ee0ceb40f66a0e3fb0eda0913390e4d527a75
Tunde Ajayi <47369345+tunde99@users.noreply.github.com>
2021-11-17 18:24:25 +0000

    Update createconfig.sh

--- joeytrainer/createconfig.sh
+++ joeytrainer/createconfig.sh
@@ -1,11 +1,11 @@
 #create config file to train a model over the JW300 en-yo corpus using 10k sp vocabulary 
-python scripts/createconfig.py --source_lang fr --target_lang wol --task lafandFrWol10 --model_path  ./joeynmt/ --data_path ./data/frwol/spdata/10k/JW/
+python scripts/createconfig.py --source_lang fr --target_lang wol --task lafandFrWol10 --model_path  /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/ --data_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/10k/JW/
 
 # reverse for 10k wol-->fr
-python scripts/createconfig.py --source_lang wol --target_lang fr --task lafandWolFr10 --model_path  ./joeynmt/ --data_path ./data/frwol/spdata/10k/JW/
+python scripts/createconfig.py --source_lang wol --target_lang fr --task lafandWolFr10 --model_path  /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/ --data_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/10k/JW/
 
 #for 20k
-python scripts/createconfig.py --source_lang fr --target_lang wol --task lafandFrWol20 --model_path  ./joeynmt/ --data_path ./data/frwol/spdata/20k/JW/
+python scripts/createconfig.py --source_lang fr --target_lang wol --task lafandFrWol20 --model_path  /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/ --data_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/20k/JW/
 
 # reverse for 20k wol-->fr
-python scripts/createconfig.py --source_lang wol --target_lang fr --task lafandWolFr20 --model_path  ./joeynmt/ --data_path ./data/frwol/spdata/20k/JW/
+python scripts/createconfig.py --source_lang wol --target_lang fr --task lafandWolFr20 --model_path  /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/joeynmt/ --data_path /content/gdrive/MyDrive/masakhane/lafand/lafand-mt/joeytrainer/data/frwol/spdata/20k/JW/
